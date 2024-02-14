a21118b1fa480fb289a4412c220f6c7bed6d5372
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-14 15:09:18 -0300

    conversão dos sockets em uma unica classe

--- cli_chat.py
+++ cli_chat.py
@@ -1,8 +1,17 @@
 import socket
 import threading
 
-class CliSocket:
-  def __init__(self, endereco_local, endereco_destino):
+class CliChatSocket:
+  '''## class ChatSocket
+  Classe com a implementação dos sockets para a comunicação em texto, e as threads de envio e recebimento das mensagens
+
+  ### Parametros
+  endereco_local : tuple[str, int]
+      tupla contendo a string que representa o IP da maquina atual, e a porta que receberá a comunicação
+  endereco_destino : tuple[str, int]
+      tupla contendo a string que representa o IP da maquina de destino, e a porta que receberá a comunicação
+  '''
+  def __init__(self, endereco_local: tuple[str, int], endereco_destino: tuple[str, int]):
     self.endereco_destino = endereco_destino
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.sock.bind(endereco_local)
@@ -10,11 +19,11 @@ class CliSocket:
   def receber_mensagens(self):
     while True:
       dados, endereco = self.sock.recvfrom(1024)
-      print(f"\n[{endereco}]: {dados.decode()}", end="\n>")
+      print(f"\n[{endereco}]: {dados.decode()}", end="\n> ")
 
   def enviar_mensagem(self):
     while True:
-      mensagem = input(">")
+      mensagem = input("> ")
       self.sock.sendto(mensagem.encode(), self.endereco_destino)
 
   def iniciar(self):
@@ -22,18 +31,3 @@ class CliSocket:
     thread_recebimento.daemon = True
     thread_recebimento.start()
     self.enviar_mensagem()
-
-def main():
-  endereco_local_servidor = ('localhost', 12345)
-  endereco_local_cliente = ('localhost', 54321)
-
-  cliente = CliSocket(endereco_local_cliente, endereco_local_servidor)
-
-  thread_cliente = threading.Thread(target=cliente.iniciar)
-  thread_cliente.daemon = True
-  thread_cliente.start()
-
-  thread_cliente.join()
-
-if __name__ == "__main__":
-  main()
