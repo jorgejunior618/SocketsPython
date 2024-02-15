98fafd4161f4b81e3fcd1603be2204635fe5c44a
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-15 11:26:36 -0300

    definindo o programa do jogo por turnos

--- jogo.py
+++ jogo.py
@@ -3,17 +3,33 @@ from models.cli_tabuleiro import CliTabuleiroSocket
 
 def iniciaJogo():
   # endereco_local = (input("IP local: "), int(input("Porta: ")))
-  endereco_local = ('localhost'), int(input("Porta: "))
+  endereco_local = ('localhost', int(input("Porta: ")))
   # endereco_destino = (input("IP destino: "), int(input("Porta: ")))
-  endereco_destino = ('localhost'), int(input("Porta: "))
+  endereco_destino = ('localhost', int(input("Porta: ")))
 
-  servidor = CliTabuleiroSocket(endereco_local, endereco_destino)
+  sockJogo = CliTabuleiroSocket(endereco_local, endereco_destino)
 
-  thread_servidor = threading.Thread(target=servidor.iniciar)
-  thread_servidor.daemon = True
-  thread_servidor.start()
+  print(" Instruções:")
+  print(" - Caso você inicie a partida:")
+  print("     Digite \"1\" e seu adversário deverá digitar \"2\"")
+  print(" - Caso seu adversário inicie a partida:")
+  print("     Digite \"2\" e seu adversário deverá digitar \"1\"")
 
-  thread_servidor.join() # Caso a thread seja interrompida
+  while True:
+    turnoLocal = sockJogo.definirTurno()
+    turnoAdversario = sockJogo.receberTurno()
+
+    while turnoLocal == turnoAdversario:
+      print(" Defina com seu colega pelo chat quem irá iniciar a partida")
+      turnoLocal = sockJogo.definirTurno()
+      turnoAdversario = sockJogo.receberTurno()
+    
+    if turnoLocal == 1:
+      print("\n Você iniciará a partida\n")
+      sockJogo.iniciarJogador1()
+    else:
+      print("\n Aguarde o lance do seu adversário\n")
+      sockJogo.iniciarJogador2()
 
 if __name__ == "__main__":
   iniciaJogo()
