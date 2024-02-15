ca8004af37e20961abe1bf75abad5108b5100cbc
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-15 10:44:44 -0300

    metodos para definição de turnos

--- models/cli_tabuleiro.py
+++ models/cli_tabuleiro.py
@@ -1,5 +1,4 @@
 import socket
-import threading
 import rascunho.resta_um_terminal as r1
 
 class CliTabuleiroSocket:
@@ -20,38 +19,82 @@ class CliTabuleiroSocket:
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.sock.bind(endereco_local)
 
-  def receber_lance(self):
+  def defineTurno(self):
+    turno, _ = self.sock.recvfrom(1024)
+    print(f" Seu adversário escolheu ser o jogador {turno}")
+    return int(turno)
+
+  def recebeTurno(self):
+    turno = input("\n Sua opção: ")
+    while turno != '1' and turno != '2':
+      turno = input(" Digite apenas 1 ou 2: ")
+
+    self.sock.sendto(turno, self.endereco_destino)
+    turno = int(turno)
+    return turno
+
+  def receberLance(self):
     '''## receber_lance()
     Recebe o lance do adversário via Socket
     '''
-    while True:
-      dados, _ = self.sock.recvfrom(1024)
+    dados, _ = self.sock.recvfrom(1024)
+    movimento = dados.decode()
 
-      movimento = dados.decode()
-      print(f"\n[adversário]: {movimento}")
-      mover, retirar = self.jogo.recebeMovimento(movimento)
-      _, destino = self.jogo.movimentoValido(mover, retirar)
-      self.jogo.fazMovimento(mover, retirar, destino)
-      self.jogo.imprimeTabuleiro()
+    print(f"\n [adversário]: {movimento}")
+    mover, retirar = self.jogo.recebeMovimento(movimento)
+    _, destino = self.jogo.movimentoValido(mover, retirar)
+    self.jogo.fazMovimento(mover, retirar, destino)
+    self.jogo.imprimeTabuleiro()
 
-  def enviar_lance(self):
+  def enviarLance(self):
     '''## enviar_lance()
     Recebe o lance e envia para o adversário via Socket
     '''
-    while True:
-      movimento = input("Seu movimento: ")
-      mover, retirar = self.jogo.recebeMovimento(movimento)
+    movimento = input(" Seu movimento: ")
+    mover, retirar = self.jogo.recebeMovimento(movimento)
+    valido, destino = self.jogo.movimentoValido(mover, retirar)
+
+    while(not valido):
       valido, destino = self.jogo.movimentoValido(mover, retirar)
 
-      while(not valido):
-        valido, destino = self.jogo.movimentoValido(mover, retirar)
+    self.jogo.fazMovimento(mover, retirar, destino)
+    self.jogo.imprimeTabuleiro()
+    self.sock.sendto(movimento.encode(), self.endereco_destino)
 
-      self.jogo.fazMovimento(mover, retirar, destino)
-      self.jogo.imprimeTabuleiro()
-      self.sock.sendto(movimento.encode(), self.endereco_destino)
+  def iniciarJogador1(self):
+    self.jogo.imprimeTabuleiro()
 
-  def iniciar(self):
-    thread_recebimento = threading.Thread(target=self.receber_lance)
-    thread_recebimento.daemon = True
-    thread_recebimento.start()
-    self.enviar_lance()
+    acabou, contPecas = self.jogo.estaNoFim()
+    while not acabou:
+      self.enviarLance()
+      acabou, contPecas = self.jogo.estaNoFim()
+      if acabou: break
+      self.receberLance()
+      acabou, contPecas = self.jogo.estaNoFim()
+
+    self.jogo.imprimeTabuleiro(self.jogo.tabuleiro)
+    if contPecas > 1:
+      print(f"\n Peças restantes: {contPecas}")
+      print(f" O vencedor é do jogador {2 - self.jogo.turno}\n")
+    else:
+      print(f" Parabens restou {contPecas}!")
+      print(f" O vencedor é do jogador {self.jogo.turno + 1}\n")
+
+  def iniciarJogador2(self):
+    self.jogo.imprimeTabuleiro()
+
+    acabou, contPecas = self.jogo.estaNoFim()
+    while not acabou:
+      self.receberLance()
+      acabou, contPecas = self.jogo.estaNoFim()
+      if acabou: break
+      self.enviarLance()
+      acabou, contPecas = self.jogo.estaNoFim()
+
+    self.jogo.imprimeTabuleiro(self.jogo.tabuleiro)
+    if contPecas > 1:
+      print(f"\n Peças restantes: {contPecas}")
+      print(f" O vencedor é do jogador {2 - self.jogo.turno}\n")
+    else:
+      print(f" Parabens restou {contPecas}!")
+      print(f" O vencedor é do jogador {self.jogo.turno + 1}\n")
