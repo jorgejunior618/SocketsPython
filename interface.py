b5c8630c6efd2376bef9a00be5f9e1cf963c92a7
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-18 15:41:57 -0300

    adição de efeitos sonoros, seleção de peças, tratamento de movimentos e conexão multiplayer

--- interface.py
+++ interface.py
@@ -1,50 +1,132 @@
 from tkinter import *
 from tkinter import ttk
+import threading
+from pygame import mixer
+
 from models.cli_tabuleiro import CliTabuleiroSocket
 
 class RestaUmInterface:
+  ''' # RestaUmInterface
+
+  Classe que inicializa uma interface grafica, com `TKinter`, para o jogo Resta Um.
+
+  ## Parâmetros:
+    cli_tabuleiro : CliTabuleiroSocket
+        O cliente socket ja inicializado que será utilizado para a conexão multiplayer
+    '''
   jogada = ""
+  pecaDestacada = -1
   meuTurno = True
   img_tabuleiro = None
-  img_nada = None
   img_peca = None
   img_vazio = None
+  img_peca_high = None
+  dictImgs = {
+    'peca': '2',
+    'vazio': '3',
+    'destacada': '4',
+  }
 
-  @staticmethod
-  def selecionaPeca(x: int, y: int, tag: int, ct) -> bool:
-    posicaoJogada = ["","a", "b", "c", "d", "e", "f", "g",""]
-    pecaSelecionada = f"{posicaoJogada[x]}{y}"
-    if len(RestaUmInterface.jogada) == 0:
-      RestaUmInterface.jogada = f"{pecaSelecionada}"
-      return False
+  def __init__(self, cli_tabuleiro: CliTabuleiroSocket):
+    '''
+    Inicializa a interface grafica do jogo
 
-    RestaUmInterface.jogada = f"{RestaUmInterface.jogada} {pecaSelecionada}"
-    RestaUmInterface.meuTurno = not ct.enviarLance(RestaUmInterface.jogada)
-    RestaUmInterface.jogada = ""
-    if RestaUmInterface.meuTurno:
-      return False
-    return True
-    # canvas.itemconfigure(tag, image=img_vazio)
-
-  def fazJogada(self, x: int, y: int, tag: int):
-    reposicionar = RestaUmInterface.selecionaPeca(x, y, tag, self.cliTab)
-    if reposicionar:
-      self.reposicionaPecas()
-
-  def __init__(self, cli_tabuleiro):
-    self.cliTab = cli_tabuleiro
+    ## Parâmetros:
+    cli_tabuleiro : CliTabuleiroSocket
+        O cliente socket ja inicializado que será utilizado para a conexão multiplayer
+    '''
+    self.cliTab: CliTabuleiroSocket = cli_tabuleiro
     self.cliTab.jogo.reiniciaTabuleiro()
     self.criaJanela()
 
     RestaUmInterface.img_tabuleiro = PhotoImage(file="assets/tabuleiro.png")
-    RestaUmInterface.img_nada = PhotoImage(file="assets/nada.png")
     RestaUmInterface.img_peca = PhotoImage(file="assets/peca.png")
     RestaUmInterface.img_vazio = PhotoImage(file="assets/vazio.png")
+    RestaUmInterface.img_peca_high = PhotoImage(file="assets/peca_highlight.png")
 
     self.criaTabuleiro()
     self.configuraEstilos()
     self.criaPecas()
-  
+
+  @staticmethod
+  def reproduzSom(efeito: str):
+    ''' ## reproduzSom(efeito)
+
+    Reproduz um efeito sonoro referente ao movimento/ação do jogo
+
+    ## Parâmetro:
+    efeito : str
+        A ação realizada, gatilho para o efeito sonoro
+    '''
+    efeitos = {
+      'derrota': 'derrota_1',
+      'vitoria': 'vitoria_1',
+      'movimento': 'movimento_1',
+      'mov_erro': 'mov_erro_2',
+    }
+    mixer.init()
+    mixer.music.load(f"assets/{efeitos[efeito]}.mp3")
+    mixer.music.set_volume(0.7)
+    mixer.music.play()
+
+  @staticmethod
+  def selecionaPeca(
+    x: int,
+    y: int,
+    tag: int,
+    ct: CliTabuleiroSocket
+  ) -> tuple[bool, int, bool]:
+    posicaoJogada = ["","a", "b", "c", "d", "e", "f", "g",""]
+    pecaSelecionada = f"{posicaoJogada[x]}{y}"
+    removerDestaque = RestaUmInterface.pecaDestacada
+
+    if (removerDestaque == tag):
+      RestaUmInterface.jogada = ""
+      RestaUmInterface.pecaDestacada = -1
+      return False, tag, False
+    RestaUmInterface.pecaDestacada = tag
+
+    if len(RestaUmInterface.jogada) == 0:
+      RestaUmInterface.jogada = f"{pecaSelecionada}"
+      return False, removerDestaque, False
+
+    RestaUmInterface.jogada = f"{RestaUmInterface.jogada} {pecaSelecionada}"
+    meuTurno = not ct.enviarLance(RestaUmInterface.jogada)
+
+    if meuTurno:
+      RestaUmInterface.pecaDestacada = tag
+      RestaUmInterface.jogada = pecaSelecionada
+      return False, removerDestaque, True
+
+    RestaUmInterface.jogada = ""
+    RestaUmInterface.pecaDestacada = -1
+    return True, removerDestaque, False
+
+  def fazJogada(self, x: int, y: int, tag: int):
+    if self.cliTab.jogo.turno == 0:
+      pecaID = self.canvas.itemcget(tag, 'image')[-1]
+      if pecaID != '3':
+        reposicionar, rmDst, movErrado = RestaUmInterface.selecionaPeca(x, y, tag, self.cliTab)
+        if RestaUmInterface.pecaDestacada != -1:
+          self.canvas.itemconfigure(tag, image=RestaUmInterface.img_peca_high)
+        if rmDst != -1:
+          self.canvas.itemconfigure(rmDst, image=RestaUmInterface.img_peca)
+
+        if reposicionar:
+          self.reposicionaPecas()
+          RestaUmInterface.reproduzSom('movimento')
+        if movErrado:
+          RestaUmInterface.reproduzSom('mov_erro')
+
+  def recebeJogadaAdversario(self):
+    while True:
+      print("esperando adversario")
+      recebeu = self.cliTab.receberLance()
+      if recebeu:
+        RestaUmInterface.meuTurno = True
+        self.reposicionaPecas()
+        RestaUmInterface.reproduzSom('movimento')
+
   def criaJanela(self):
     self.janela = Tk()
     self.janela.title("Resta um")
@@ -95,8 +177,18 @@ class RestaUmInterface:
   def iniciaAplicacao(self):
     self.janela.mainloop()
 
+# Inicializando o socket do cliente do jogador
 ct = CliTabuleiroSocket(('localhost', 1234), ('localhost', 4321))
+
+# Inicializando o objeto que instanciará o jogo
 meuJogo = RestaUmInterface(ct)
+
+# Criando a thread que recebe a jogada do adversário via Socket
+thread_jogo = threading.Thread(target=meuJogo.recebeJogadaAdversario)
+thread_jogo.daemon = True
+thread_jogo.start()
+
+# Inicializando a aplicação grafica do jogo
 meuJogo.iniciaAplicacao()
 
 # feet = StringVar()
