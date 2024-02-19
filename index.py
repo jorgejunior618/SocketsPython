97b8de8b207e3ff2e68c94c037ae0c592fc6494b
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-19 09:37:24 -0300

    utilizando arquivo centralizado para execução dos processos

--- index.py
+++ index.py
@@ -1,6 +1,20 @@
 import chat as chatService
 import jogo as jogoService
+from models.cli_tabuleiro import CliTabuleiroSocket
+from gui_resta_um import GuiRestaUm
 import threading
 
 if __name__ == "__main__":
-  pass
+  # Inicializando o socket do cliente do jogador
+  ct = CliTabuleiroSocket(('localhost', 1234), ('localhost', 4321))
+
+  # Inicializando o objeto que instanciará o jogo
+  meuJogo = GuiRestaUm(ct)
+
+  # Criando a thread que recebe a jogada do adversário via Socket
+  thread_jogo = threading.Thread(target=meuJogo.recebeJogadaAdversario)
+  thread_jogo.daemon = True
+  thread_jogo.start()
+
+  # Inicializando a aplicação grafica do jogo
+  meuJogo.iniciaAplicacao()
