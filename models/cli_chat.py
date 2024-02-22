366fca8592268417eb7ba400678480e8b1f09f9d
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-22 15:16:03 -0300

    adaptação ao chat na interface grafica

--- models/cli_chat.py
+++ models/cli_chat.py
@@ -16,18 +16,17 @@ class CliChatSocket:
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     self.sock.bind(endereco_local)
 
-  def receber_mensagens(self):
-    while True:
-      dados, endereco = self.sock.recvfrom(1024)
-      print(f"\n[{endereco}]: {dados.decode()}", end="\n> ")
+  def receber_mensagen(self) -> str:
+    dados, endereco = self.sock.recvfrom(1024)
+    print(f"\n[{endereco}]: {dados.decode()}", end="\n> ")
+    return dados
 
-  def enviar_mensagem(self):
-    while True:
-      mensagem = input("> ")
-      self.sock.sendto(mensagem.encode(), self.endereco_destino)
+  def enviar_mensagem(self, mensagem: str):
+    dados = mensagem.encode()
+    self.sock.sendto(dados, self.endereco_destino)
 
   def iniciar(self):
-    thread_recebimento = threading.Thread(target=self.receber_mensagens)
+    thread_recebimento = threading.Thread(target=self.receber_mensagen)
     thread_recebimento.daemon = True
     thread_recebimento.start()
     self.enviar_mensagem()
