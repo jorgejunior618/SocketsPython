068986f9a3ffb05c47bbb9f3c4ba0f74e57a55d6
jorgejunior618 <jorgejunior618@gmail.com>
2024-02-24 13:41:33 -0300

    centralização da função principal para execução do programa

--- index.py
+++ index.py
@@ -1,17 +1,29 @@
-import chat as chatService
-import jogo as jogoService
 from models.cli_tabuleiro import CliTabuleiroSocket
 from models.cli_chat import CliChatSocket
 from gui_resta_um import GuiRestaUm
-import threading
+from ip_helper import IPHelper
+from gui_define_ip import GuiDefineAdversario
 
-if __name__ == "__main__":
+def mainApp():
   # Inicializando o socket do cliente do jogador
-  ct = CliTabuleiroSocket(('localhost', 1234), ('localhost', 4321))
-  cc = CliChatSocket(('localhost', 12345), ('localhost', 54321))
+  iph = IPHelper()
+  gdefAdv = GuiDefineAdversario(iph)
+  gdefAdv.iniciaAplicacao()
+  if iph.ipAdversario == None:
+    return None
+  
+  print("CliJogo: ",iph.defineEnderecoLocalJogo(),iph.defineEnderecoAdversarioJogo(),)
+  print("CliChat: ",iph.defineEnderecoLocalChat(),iph.defineEnderecoAdversarioChat(),)
+
+  ct = CliTabuleiroSocket(iph.defineEnderecoLocalJogo(), iph.defineEnderecoAdversarioJogo())
+  cc = CliChatSocket(iph.defineEnderecoLocalChat(), iph.defineEnderecoAdversarioChat())
 
   # Inicializando o objeto que instanciará o jogo
   meuJogo = GuiRestaUm(ct, cc)
 
   # Inicializando a aplicação grafica do jogo
   meuJogo.iniciaAplicacao()
+
+
+if __name__ == "__main__":
+  mainApp()
